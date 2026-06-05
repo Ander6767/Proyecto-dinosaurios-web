b9b5fa4518c0428145667355d4db52e56ad70c77
Ander6767 <andersantiagoinfantemontiel@gmail.com>
2026-06-05 16:13:45 -0600

    Proyecto Dinosaurios

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB conectado"))
+.catch(err => console.log(err));
+
+const DinosaurioSchema = new mongoose.Schema({
+    nombre: String,
+    especie: String,
+    periodo: String,
+    dieta: String
+});
+
+const Dinosaurio = mongoose.model(
+    "Dinosaurio",
+    DinosaurioSchema
+);
+
+
+// Obtener todos
+app.get('/dinosaurios', async(req,res)=>{
+    const datos = await Dinosaurio.find();
+    res.json(datos);
+});
+
+// Crear
+app.post('/dinosaurios', async(req,res)=>{
+    const nuevo = new Dinosaurio(req.body);
+    await nuevo.save();
+    res.json(nuevo);
+});
+
+// Editar
+app.put('/dinosaurios/:id', async(req,res)=>{
+    const actualizado =
+    await Dinosaurio.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    );
+
+    res.json(actualizado);
+});
+
+// Eliminar
+app.delete('/dinosaurios/:id', async(req,res)=>{
+    await Dinosaurio.findByIdAndDelete(
+        req.params.id
+    );
+
+    res.json({
+        mensaje:"Eliminado"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor en puerto ${PORT}`);
+});
---
+++ frontend/script.js
@@ -0,0 +1,80 @@
+const API =
+"http://localhost:3000/dinosaurios";
+
+async function cargar(){
+
+    const res = await fetch(API);
+    const datos = await res.json();
+
+    let html = "";
+
+    datos.forEach(d => {
+
+        html += `
+        <div class="card">
+
+        <h3>${d.nombre}</h3>
+
+        <p>Especie:
+        ${d.especie}</p>
+
+        <p>Periodo:
+        ${d.periodo}</p>
+
+        <p>Dieta:
+        ${d.dieta}</p>
+
+        <button onclick="eliminar('${d._id}')">
+        Eliminar
+        </button>
+
+        </div>
+        `;
+    });
+
+    document.getElementById(
+        "lista"
+    ).innerHTML = html;
+}
+
+async function agregar(){
+
+    const nombre =
+    document.getElementById("nombre").value;
+
+    const especie =
+    document.getElementById("especie").value;
+
+    const periodo =
+    document.getElementById("periodo").value;
+
+    const dieta =
+    document.getElementById("dieta").value;
+
+    await fetch(API,{
+        method:"POST",
+        headers:{
+            "Content-Type":
+            "application/json"
+        },
+        body:JSON.stringify({
+            nombre,
+            especie,
+            periodo,
+            dieta
+        })
+    });
+
+    cargar();
+}
+
+async function eliminar(id){
+
+    await fetch(API + "/" + id,{
+        method:"DELETE"
+    });
+
+    cargar();
+}
+
+cargar();
