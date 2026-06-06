cfa383ad434e878e9807ebe8c3202882a46c58c9
Ander6767 <andersantiagoinfantemontiel@gmail.com>
2026-06-05 23:04:46 -0600

    Conectar frontend con Express

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("MongoDB conectado"))
@@ -63,7 +65,9 @@ app.delete('/dinosaurios/:id', async(req,res)=>{
 });
 
 const PORT = process.env.PORT || 3000;
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 app.listen(PORT, ()=>{
     console.log(`Servidor en puerto ${PORT}`);
 });
